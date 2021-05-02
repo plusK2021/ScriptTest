74d0cd68c167776390e3759b76059da162faf046
plusK2021 <81849327+plusK2021@users.noreply.github.com>
2021-05-02 10:10:04 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //配列の初期化
+        int[] array = { 50, 30, 80, 100, 20 };
+
+        //配列の各要素を順番に表示させる
+        for (int i = 0; i <= 4; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //配列の各要素を逆順に表示させる
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TestB.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss
+{
+    //int型の変数mpを宣言し、53で初期化
+    private int mp = 53;
+
+    //魔法攻撃用の関数
+    public void Magic()
+    {
+            if (mp >= 5)
+            {
+                this.mp -= 5;
+
+                Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+            }
+
+            else
+            {
+                Debug.Log("MPが足りないため魔法が使えない。");
+            }
+    }
+}
+
+
+public class TestB : MonoBehaviour
+{
+    void Start ()
+    {
+        Boss lastboss = new Boss();
+
+        for (int i = 0; i < 11; i++)
+        {
+            lastboss.Magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
